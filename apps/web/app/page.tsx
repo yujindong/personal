b06256b602f4d177081341cc1b8ebfa959667fc2
yujindong <yujindong@yujindongs-Mac-mini.local>
2025-04-22 09:55:31 +0800

    测试ci

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome web 👋
+              Welcome web 👋1
             </h1>
           </div>
 
